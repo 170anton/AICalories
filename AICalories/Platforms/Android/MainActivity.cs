1e75a9ca48d2633fe4294e8f6fb294f49691821d
Anton Jakubau <170anton@gmail.com>
2024-08-28 16:15:24 +0200

    Release configurations

--- AICalories/Platforms/Android/MainActivity.cs
+++ AICalories/Platforms/Android/MainActivity.cs
@@ -7,9 +7,6 @@ namespace AICalories;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
-    {
-        Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
+
+
 }
